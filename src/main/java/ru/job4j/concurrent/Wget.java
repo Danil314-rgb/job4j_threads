51e869ef96ff3b016a3fa77129e5425a69ef1ffa
Danil <totem7h@gmail.com>
2022-02-27 14:28:02 +0400

    4. Скачивание файла с ограничением. [#144271 #269755]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -5,6 +5,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 
+import static java.lang.System.currentTimeMillis;
+
 public class Wget implements Runnable {
 
     private final String url;
@@ -18,12 +20,27 @@ public class Wget implements Runnable {
     @Override
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp123.dat")) {
             byte[] dataBuffer = new byte[1024];
             int bytesRead;
+            int downloadData = 0;
+            long time1 = currentTimeMillis();
+            System.out.println("Стартовый момент: " + time1 + " миллисекунд");
             while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                downloadData += bytesRead;
+                if (downloadData == speed) {
+                    long time2 = currentTimeMillis();
+                    System.out.println("Текущий момент: " + time2 + " миллисекунд");
+                    long res = time2 - time1;
+                    System.out.println("Разница: " + res + " миллисекунд");
+                    if (res < 1000) {
+                        Thread.sleep(1000 - res);
+                        downloadData = 0;
+                        time1 = currentTimeMillis();
+                    }
+                }
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
-                Thread.sleep(1000);
+                /*Thread.sleep(1000);*/
             }
         } catch (IOException | InterruptedException e) {
             Thread.currentThread().interrupt();
@@ -31,8 +48,8 @@ public class Wget implements Runnable {
     }
 
     public static void main(String[] args) throws InterruptedException {
-        String url = args[0];
-        int speed = Integer.parseInt(args[1]);
+        String url = "https://proof.ovh.net/files/10Mb.dat"; /*args[0];*/
+        int speed = 1024; /*Integer.parseInt(args[1]);*/
         Thread wget = new Thread(new Wget(url, speed));
         wget.start();
         wget.join();
