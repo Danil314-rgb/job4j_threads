df1c405a7a99981e2d50cf31ae2ddf748b76996d
Danil <totem7h@gmail.com>
2022-03-02 14:57:49 +0400

    1. Реализовать шаблон Producer Consumer. [#1098]

---
+++ src/test/java/ru/job4j/concurrent/wait/SimpleBlockingQueueTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.concurrent.wait;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SimpleBlockingQueueTest {
+
+    @Test
+    public void when() throws InterruptedException {
+        SimpleBlockingQueue queue = new SimpleBlockingQueue(2);
+        Thread first = new Thread(() -> {
+            try {
+                queue.offer(1);
+                queue.offer(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        Thread second = new Thread(() -> {
+            try {
+                queue.poll();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        first.start();
+        second.start();
+        first.join();
+        second.join();
+    }
+}
