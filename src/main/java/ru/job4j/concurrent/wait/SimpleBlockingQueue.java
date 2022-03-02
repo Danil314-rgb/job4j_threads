030504902628e738f2baee51200c408184a07413
Danil <totem7h@gmail.com>
2022-03-02 20:42:38 +0400

    1. Реализовать шаблон Producer Consumer. [#1098 #271012]

--- src/main/java/ru/job4j/concurrent/wait/SimpleBlockingQueue.java
+++ src/main/java/ru/job4j/concurrent/wait/SimpleBlockingQueue.java
@@ -20,8 +20,8 @@ public class SimpleBlockingQueue<T> {
     public synchronized void offer(T value) throws InterruptedException {
         while (queue.size() == limit) {
             wait();
-            queue.add(value);
         }
+        queue.add(value);
         notifyAll();
     }
 
